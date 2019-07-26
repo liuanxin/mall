190a3a5f4d92ef8cc850953adcdcdfabc05a98b2
liujie <liu_anxin@163.com>
2019-07-26 15:11:48 +0800

    fix

--- mall-common/src/main/java/com/github/common/enums/TestEnum.java
+++ mall-common/src/main/java/com/github/common/enums/TestEnum.java
@@ -2,5 +2,10 @@ package com.github.common.enums;
 
 public enum TestEnum {
 
-    One, Two, Three;
+    One(1), Two(2), Three(3);
+
+    int code;
+    TestEnum(int code) { this.code = code; }
+
+    public int getCode() { return code; }
 }
