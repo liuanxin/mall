d5644e94e423ef8657ddfb54b29ee376988fa863
liujie <liu_anxin@163.com>
2019-12-05 10:49:57 +0800

    fix

--- web-manager/src/main/java/com/github/vo/ManagerUserVo.java
+++ web-manager/src/main/java/com/github/vo/ManagerUserVo.java
@@ -6,9 +6,11 @@ import com.github.common.util.U;
 import com.github.liuanxin.api.annotation.ApiReturn;
 import com.github.manager.model.ManagerMenu;
 import com.github.manager.model.ManagerUser;
+import com.google.common.collect.Lists;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Data
@@ -55,6 +57,60 @@ public class ManagerUserVo {
                 vo.setMenus(menuVos);
             }
         }
+        return vo;
+    }
+    public static ManagerUserVo testData() {
+        ManagerUserVo vo = new ManagerUserVo();
+        vo.setId(123L);
+        vo.setUserName("zhanshan");
+        vo.setNickName("张三");
+
+        List<ManagerMenuVo> menus = Lists.newArrayList();
+
+        List<ManagerMenuVo> commonList = Arrays.asList(
+                new ManagerMenuVo().setName("全局配置").setFront("config-index"),
+                new ManagerMenuVo().setName("添加全局配置").setFront("config-add"),
+                new ManagerMenuVo().setName("编辑全局配置").setFront("config-edit"),
+
+                new ManagerMenuVo().setName("banner").setFront("banner-index"),
+                new ManagerMenuVo().setName("添加 banner").setFront("banner-add"),
+                new ManagerMenuVo().setName("编辑 banner").setFront("banner-edit")
+        );
+        menus.add(new ManagerMenuVo().setName("公共管理").setFront("common").setChildren(commonList));
+
+        List<ManagerMenuVo> userList = Arrays.asList(
+                new ManagerMenuVo().setName("用户列表").setFront("user-index"),
+                new ManagerMenuVo().setName("添加用户").setFront("user-add"),
+                new ManagerMenuVo().setName("编辑用户").setFront("user-edit")
+        );
+        menus.add(new ManagerMenuVo().setName("用户管理").setFront("user").setChildren(userList));
+
+        List<ManagerMenuVo> orderList = Arrays.asList(
+                new ManagerMenuVo().setName("商品列表").setFront("product-index"),
+                new ManagerMenuVo().setName("添加商品").setFront("product-add"),
+                new ManagerMenuVo().setName("编辑商品").setFront("product-edit")
+        );
+        menus.add(new ManagerMenuVo().setName("商品管理").setFront("product").setChildren(orderList));
+
+        List<ManagerMenuVo> productList = Arrays.asList(
+                new ManagerMenuVo().setName("订单列表").setFront("order-index"),
+                new ManagerMenuVo().setName("添加订单").setFront("order-add"),
+                new ManagerMenuVo().setName("编辑订单").setFront("order-edit")
+        );
+        menus.add(new ManagerMenuVo().setName("订单管理").setFront("order").setChildren(productList));
+
+        List<ManagerMenuVo> managerList = Arrays.asList(
+                new ManagerMenuVo().setName("人员列表").setFront("account-index"),
+                new ManagerMenuVo().setName("添加人员").setFront("account-add"),
+                new ManagerMenuVo().setName("编辑人员").setFront("account-edit"),
+
+                new ManagerMenuVo().setName("角色列表").setFront("role-index"),
+                new ManagerMenuVo().setName("添加角色").setFront("role-add"),
+                new ManagerMenuVo().setName("编辑角色").setFront("role-edit")
+        );
+        menus.add(new ManagerMenuVo().setName("系统管理").setFront("manager").setChildren(managerList));
+
+        vo.setMenus(menus);
         return vo;
     }
 }
