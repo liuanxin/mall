fc2fdb33f5c94aa9861089cb05efec99d2cb5d9b
Liu Jie <zt17947@zongteng.net>
2020-12-30 14:06:37 +0800

    fix

--- mall-common/src/main/java/com/github/common/sql/ShowSql5Interceptor.java
+++ mall-common/src/main/java/com/github/common/sql/ShowSql5Interceptor.java
@@ -13,6 +13,7 @@
 //import java.sql.SQLException;
 //import java.util.Properties;
 //import java.util.concurrent.TimeUnit;
+//import java.util.concurrent.atomic.AtomicLong;
 //
 ///**
 // * mysql 5 的连接参数是: &statementInterceptors=com.github.common.sql.ShowSql5Interceptor
@@ -20,57 +21,83 @@
 // */
 //public class ShowSql5Interceptor implements StatementInterceptor {
 //
+//    private static final String TIME_SPLIT = " + ";
+//    private static final AtomicLong COUNTER = new AtomicLong(0L);
 //    /** 每条 sql 执行前记录时间戳, 如果使用 ThreadLocal 会有 pre 了但运行时异常不去 post 的情况 */
-//    private static final Cache<Thread, Long> TIME_CACHE = CacheBuilder.newBuilder().expireAfterWrite(60, TimeUnit.MINUTES).build();
+//    private static final Cache<Thread, String> TIME_CACHE = CacheBuilder.newBuilder()
+//            .expireAfterWrite(60, TimeUnit.MINUTES).build();
 //
 //    @Override
 //    public void init(Connection connection, Properties properties) throws SQLException {}
 //
 //    @Override
-//    public ResultSetInternalMethods preProcess(String sql, Statement statement,
-//                                               Connection connection) throws SQLException {
+//    public ResultSetInternalMethods preProcess(String sql, Statement statement, Connection connection) throws SQLException {
 //        if (LogUtil.SQL_LOG.isDebugEnabled()) {
-//            TIME_CACHE.put(Thread.currentThread(), System.currentTimeMillis());
+//            String realSql = getRealSql(sql, statement);
+//            if (U.isNotBlank(realSql)) {
+//                Thread currentThread = Thread.currentThread();
+//                long counter = COUNTER.addAndGet(1);
+//                long start = System.currentTimeMillis();
+//
+//                TIME_CACHE.put(currentThread, counter + TIME_SPLIT + start);
+//                LogUtil.SQL_LOG.debug("counter: {}, sql: {}", counter, realSql);
+//            }
 //        }
 //        return null;
+//    }
+//
+//    private String getRealSql(String sql, Statement statement) {
+//        if (U.isBlank(sql)) {
+//            if (U.isNotBlank(statement)) {
+//                sql = statement.toString();
+//                if (U.isNotBlank(sql)) {
+//                    int i = sql.indexOf(':');
+//                    if (i > 0) {
+//                        sql = sql.substring(i + 1).trim();
+//                    }
+//                }
+//            }
+//        }
+//        if (U.isNotBlank(sql)) {
+//            // druid -> SQLUtils.formatMySql
+//            sql = SqlFormat.format(sql.replaceFirst("^\\s*?\n", ""));
+//        }
+//        return sql;
 //    }
 //
 //    @Override
 //    public ResultSetInternalMethods postProcess(String sql, Statement statement, ResultSetInternalMethods resultSet,
 //                                                Connection connection) throws SQLException {
 //        if (LogUtil.SQL_LOG.isDebugEnabled()) {
-//            Thread thread = Thread.currentThread();
-//            try {
-//                if (U.isBlank(sql) && U.isNotBlank(statement)) {
-//                    sql = statement.toString();
-//                    if (U.isNotBlank(sql)) {
-//                        int i = sql.indexOf(':');
-//                        if (i > 0) {
-//                            sql = sql.substring(i + 1).trim();
+//            String realSql = getRealSql(sql, statement);
+//            if (U.isNotBlank(realSql)) {
+//                Thread currentThread = Thread.currentThread();
+//                String counterAndTime = TIME_CACHE.getIfPresent(currentThread);
+//                if (U.isNotBlank(counterAndTime)) {
+//                    try {
+//                        String[] split = counterAndTime.split(TIME_SPLIT);
+//                        long counter = U.toLong(split[0]);
+//                        long start = U.toLong(split[1]);
+//
+//                        StringBuilder sbd = new StringBuilder();
+//                        if (U.greater0(counter)) {
+//                            sbd.append("counter: ").append(counter);
 //                        }
+//                        if (U.greater0(start)) {
+//                            sbd.append(", time: ").append(DateUtil.toHuman(System.currentTimeMillis() - start));
+//                        }
+//                        if (resultSet != null && resultSet.reallyResult() && resultSet.last()) {
+//                            int size = resultSet.getRow();
+//                            resultSet.beforeFirst();
+//                            if (size > 0) {
+//                                sbd.append(", size: ").append(size).append(", ");
+//                            }
+//                        }
+//                        LogUtil.SQL_LOG.debug(sbd.toString());
+//                    } finally {
+//                        TIME_CACHE.invalidate(currentThread);
 //                    }
 //                }
-//                if (U.isNotBlank(sql)) {
-//                    StringBuilder sbd = new StringBuilder();
-//
-//                    Long start = TIME_CACHE.getIfPresent(thread);
-//                    if (U.greater0(start)) {
-//                        sbd.append("time: ").append(DateUtil.toHuman(System.currentTimeMillis() - start)).append(", ");
-//                    }
-//                    if (resultSet != null && resultSet.reallyResult() && resultSet.last()) {
-//                        int size = resultSet.getRow();
-//                        resultSet.beforeFirst();
-//                        if (size > 0) {
-//                            sbd.append("size: ").append(size).append(", ");
-//                        }
-//                    }
-//                    // druid -> SQLUtils.formatMySql
-//                    sbd.append("sql:\n").append(SqlFormat.format(sql).replaceFirst("^\\s*?\n", ""));
-//
-//                    LogUtil.SQL_LOG.debug(sbd.toString());
-//                }
-//            } finally {
-//                TIME_CACHE.invalidate(thread);
 //            }
 //        }
 //        return null;
--- mall-common/src/main/java/com/github/common/sql/ShowSql8Interceptor.java
+++ mall-common/src/main/java/com/github/common/sql/ShowSql8Interceptor.java
@@ -7,15 +7,14 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.mysql.cj.MysqlConnection;
 import com.mysql.cj.Query;
-import com.mysql.cj.ServerPreparedQuery;
 import com.mysql.cj.interceptors.QueryInterceptor;
-import com.mysql.cj.jdbc.result.ResultSetImpl;
 import com.mysql.cj.log.Log;
 import com.mysql.cj.protocol.Resultset;
 import com.mysql.cj.protocol.ServerSession;
 
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Supplier;
 
 /**
@@ -24,8 +23,11 @@ import java.util.function.Supplier;
  */
 public class ShowSql8Interceptor implements QueryInterceptor {
 
+    private static final String TIME_SPLIT = " + ";
+    private static final AtomicLong COUNTER = new AtomicLong(0L);
     /** 每条 sql 执行前记录时间戳, 如果使用 ThreadLocal 会有 pre 了但运行时异常不去 post 的情况 */
-    private static final Cache<Thread, Long> TIME_CACHE = CacheBuilder.newBuilder().expireAfterWrite(60, TimeUnit.MINUTES).build();
+    private static final Cache<Thread, String> TIME_CACHE = CacheBuilder.newBuilder()
+            .expireAfterWrite(60, TimeUnit.MINUTES).build();
 
     @Override
     public QueryInterceptor init(MysqlConnection conn, Properties props, Log log) {
@@ -33,50 +35,57 @@ public class ShowSql8Interceptor implements QueryInterceptor {
     }
 
     @Override
-    public <T extends Resultset> T preProcess(Supplier<String> sql, Query interceptedQuery) {
+    public <T extends Resultset> T preProcess(Supplier<String> sql, Query query) {
         if (LogUtil.SQL_LOG.isDebugEnabled()) {
-            TIME_CACHE.put(Thread.currentThread(), System.currentTimeMillis());
+            String realSql = getRealSql(sql);
+            if (U.isNotBlank(realSql)) {
+                Thread currentThread = Thread.currentThread();
+                long current = System.currentTimeMillis();
+                long counter = COUNTER.addAndGet(1);
+
+                TIME_CACHE.put(currentThread, counter + TIME_SPLIT + current);
+                LogUtil.SQL_LOG.debug("counter: {}, sql:\n{}", counter, realSql);
+            }
         }
         return null;
+    }
+
+    private String getRealSql(Supplier<String> sql) {
+        // druid -> SQLUtils.formatMySql
+        return U.isBlank(sql) ? null : SqlFormat.format(sql.get().replaceFirst("^\\s*?\n", ""));
     }
 
     @Override
     public <T extends Resultset> T postProcess(Supplier<String> sql, Query query, T rs, ServerSession serverSession) {
         if (LogUtil.SQL_LOG.isDebugEnabled()) {
-            Thread thread = Thread.currentThread();
-            try {
-                String realSql = U.isNotBlank(sql) ? sql.get() : "";
-                if (realSql.contains("?") && query instanceof ServerPreparedQuery && rs instanceof ResultSetImpl) {
-                    // 如果设置了 useServerPrepStmts 为 true 的话, query 将是 ServerPreparedQuery,
-                    // 此时通过下面方式获取的 sql 语句中不会有单引号('), 比如应该是 name = '张三' 的将会输出成 name = 张三
-                    // 且 insert 语句只能输出带 ? 的语句
-                    String tmp = ((ResultSetImpl) rs).getOwningQuery().toString();
+            String realSql = getRealSql(sql);
+            if (U.isNotBlank(realSql)) {
+                Thread currentThread = Thread.currentThread();
+                String counterAndTime = TIME_CACHE.getIfPresent(currentThread);
+                if (U.isNotBlank(counterAndTime)) {
+                    try {
+                        String[] split = counterAndTime.split(TIME_SPLIT);
+                        long counter = U.toLong(split[0]);
+                        long start = U.toLong(split[1]);
 
-                    String colon = ":";
-                    if (U.isNotBlank(tmp) && tmp.contains(colon)) {
-                        realSql = tmp.substring(tmp.indexOf(colon) + colon.length()).trim();
-                    }
-                }
-
-                if (U.isNotBlank(realSql)) {
-                    StringBuilder sbd = new StringBuilder();
-
-                    Long start = TIME_CACHE.getIfPresent(thread);
-                    if (U.greater0(start)) {
-                        sbd.append("time: ").append(DateUtil.toHuman(System.currentTimeMillis() - start)).append(", ");
-                    }
-                    if (U.isNotBlank(rs) && rs.hasRows()) {
-                        int size = rs.getRows().size();
-                        if (size > 0) {
-                            sbd.append("size: ").append(size).append(", ");
+                        StringBuilder sbd = new StringBuilder();
+                        if (U.greater0(counter)) {
+                            sbd.append("counter: ").append(counter);
                         }
+                        if (U.greater0(start)) {
+                            sbd.append(", time: ").append(DateUtil.toHuman(System.currentTimeMillis() - start));
+                        }
+                        if (U.isNotBlank(rs) && rs.hasRows()) {
+                            int size = rs.getRows().size();
+                            if (size > 0) {
+                                sbd.append(", size: ").append(size);
+                            }
+                        }
+                        LogUtil.SQL_LOG.debug(sbd.toString());
+                    } finally {
+                        TIME_CACHE.invalidate(currentThread);
                     }
-                    sbd.append("sql:\n").append(SqlFormat.format(realSql).replaceFirst("^\\s*?\n", ""));
-
-                    LogUtil.SQL_LOG.debug(sbd.toString());
                 }
-            } finally {
-                TIME_CACHE.invalidate(thread);
             }
         }
         return null;
