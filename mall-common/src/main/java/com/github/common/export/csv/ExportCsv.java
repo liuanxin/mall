842de596bc89bdbfdbd3554367b9be02f82fd373
Jie <zt17947@ztn.com>
2021-04-01 17:23:59 +0800

    fix

--- mall-common/src/main/java/com/github/common/export/csv/ExportCsv.java
+++ mall-common/src/main/java/com/github/common/export/csv/ExportCsv.java
@@ -9,11 +9,14 @@ import java.util.Set;
 
 public class ExportCsv {
 
-    // csv 用英文逗号(,)隔开列, 用换行(\n)隔开行, 内容中包含了逗号的需要用双引号包裹, 若内容中包含了双引号则需要用两个双引号表示
+    // 见: https://zh.wikipedia.org/wiki/%E9%80%97%E5%8F%B7%E5%88%86%E9%9A%94%E5%80%BC
+    // 用英文逗号(,)隔开列, 用换行(\n)隔开行
+    //   为避免换行处理出问题: 内容中包含了换行的替换成空格
+    //   内容中包含了双引号则替换成两个双引号, 内容中包含了逗号、空格或双引号的则用双引号包裹
 
     private static final String SPLIT = ",";
     private static final String WRAP = "\n";
-
+    private static final String SPACE = " ";
     private static final String QUOTE = "\"";
     private static final String REPLACE_QUOTE = "\"\"";
 
@@ -49,10 +52,13 @@ public class ExportCsv {
     }
 
     private static String handleCsvContent(String content) {
+        if (content.contains(WRAP)) {
+            content = content.replace(WRAP, SPACE);
+        }
         if (content.contains(QUOTE)) {
             content = content.replace(QUOTE, REPLACE_QUOTE);
         }
-        if (content.contains(SPLIT)) {
+        if (content.contains(SPLIT) || content.contains(SPACE) || content.contains(QUOTE)) {
             content = QUOTE + content + QUOTE;
         }
         return content;
