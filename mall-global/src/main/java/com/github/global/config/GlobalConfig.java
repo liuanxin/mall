7f64ea2c980b9258d1a66f9e35516db305683624
Jie <zt17947@ztn.com>
2021-06-18 14:47:45 +0800

    fix

--- mall-global/src/main/java/com/github/global/config/GlobalConfig.java
+++ mall-global/src/main/java/com/github/global/config/GlobalConfig.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.common.date.DateUtil;
 import com.github.common.util.ApplicationContexts;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,29 +24,29 @@ public class GlobalConfig {
 
     /** see: https://www.baeldung.com/spring-boot-customize-jackson-objectmapper */
     @Configuration
-    @ConditionalOnClass(ObjectMapper.class)
-    public static class JsonModule {
-
+    @ConditionalOnBean(ObjectMapper.class)
+    public static class JsonCustomModule {
         @JsonComponent
-        public static class DataJsonModule {
-            public static class Serializer extends JsonDeserializer<Date> {
+        public static class CustomJsonComponent {
+            public static class JsonDataDeserializer extends JsonDeserializer<Date> {
                 @Override
                 public Date deserialize(JsonParser p, DeserializationContext ctx) throws IOException {
                     return DateUtil.parse(p.getText().trim());
                 }
             }
         }
-
-        /*@Bean
-        public Module customModule() {
-            SimpleModule module = new SimpleModule();
-            module.addDeserializer(Date.class, new JsonDeserializer<Date>() {
-                @Override
-                public Date deserialize(JsonParser p, DeserializationContext ctx) throws IOException {
-                    return DateUtil.parse(p.getText().trim());
-                }
-            });
-            return module;
-        }*/
     }
+
+    /*@Bean
+    @ConditionalOnBean(ObjectMapper.class)
+    public Module customModule() {
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(Date.class, new JsonDeserializer<Date>() {
+            @Override
+            public Date deserialize(JsonParser p, DeserializationContext ctx) throws IOException {
+                return DateUtil.parse(p.getText().trim());
+            }
+        });
+        return module;
+    }*/
 }
