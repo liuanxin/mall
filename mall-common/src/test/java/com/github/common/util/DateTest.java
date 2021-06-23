807eff7759dde220dc6514e0dd0332c80bc804e2
Jie <zt17947@ztn.com>
2021-06-23 09:38:09 +0800

    fix

--- mall-common/src/test/java/com/github/common/util/DateTest.java
+++ mall-common/src/test/java/com/github/common/util/DateTest.java
@@ -5,6 +5,7 @@ import com.github.common.date.DateUtil;
 import org.junit.Test;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class DateTest {
 
@@ -19,11 +20,11 @@ public class DateTest {
         System.out.println(date);
         System.out.println(DateUtil.parse(date));
 
-        date = DateUtil.format(now, DateFormatType.YYYYMMDDHHMMSS);
+        date = DateUtil.format(now, DateFormatType.USA_YYYY_MM_DD_HH_MM_SS);
         System.out.println(DateUtil.parse(date));
 
-        long end = DateUtil.parse("2019-01-03 01:02:02").getTime();
-        long start = DateUtil.parse("2019-01-01 01:02:03").getTime();
+        long end = Objects.requireNonNull(DateUtil.parse("2019-01-03 01:02:02")).getTime();
+        long start = Objects.requireNonNull(DateUtil.parse("2019-01-01 01:02:03")).getTime();
         System.out.println(DateUtil.toHumanRoughly(end - start));
         System.out.println(DateUtil.toHuman(end - start));
 
