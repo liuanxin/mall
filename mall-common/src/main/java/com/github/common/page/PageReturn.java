12b3c2e62d0ddf575e3a6c031a9eadf3403b00c1
Jie <zt17947@ztn.com>
2021-06-30 14:17:09 +0800

    fix

--- mall-common/src/main/java/com/github/common/page/PageReturn.java
+++ mall-common/src/main/java/com/github/common/page/PageReturn.java
@@ -12,25 +12,6 @@ import java.io.Serializable;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * <pre>
- * 此实体类在 Controller 和 Service 中用到分页时使用.
- *
- * &#064;Controller --> request 请求中带过来的参数使用 Page 进行接收(如果前端不传, 此处接收则程序会使用默认值)
- * public JsonResult xx(xxx, PageParam page) {
- *     PageReturn pageInfo = xxxService.page(xxx, page);
- *     return success("xxx", (page.isWasMobile() ? pageInfo.getList() : pageInfo));
- * }
- *
- * &#064;Service --> 调用方法使用 Page 进行传递, 返回 PageInfo
- * public PageReturn page(xxx, PageParam page) {
- *     List&lt;XXX> xxxList = xxxMapper.selectPage(Pages.param(page), xxxxx);
- *     return Pages.returnPage(xxxList);
- * }
- *
- * 这么做的目的是分页包只需要在服务端引入即可
- * </pre>
- */
 @Setter
 @Getter
 @NoArgsConstructor
@@ -39,9 +20,6 @@ import java.util.List;
 public class PageReturn<T> implements Serializable {
     private static final long serialVersionUID = 0L;
 
-    @ApiReturn("使用 es 查询时会返回此值, 请求下一页(仅下一页, 不能跳页, 也不能上一页)时这个如果有值, 带上即可")
-    private String searchAfter;
-
     @ApiReturn("SELECT COUNT(*) FROM ... 的结果")
     private long total;
 
@@ -49,10 +27,10 @@ public class PageReturn<T> implements Serializable {
     private List<T> list;
 
     public static <T> PageReturn<T> emptyReturn() {
-        return new PageReturn<>(null, 0, Collections.emptyList());
+        return new PageReturn<>(0, Collections.emptyList());
     }
     public static <T> PageReturn<T> returnPage(long total, List<T> list) {
-        return new PageReturn<>(null, total, list);
+        return new PageReturn<>(total, list);
     }
 
     /** 在 Controller 中调用 --> 组装不同的 vo 时使用此方法 */
@@ -63,7 +41,6 @@ public class PageReturn<T> implements Serializable {
             // 只要总条数
             PageReturn<T> info = new PageReturn<>();
             info.setTotal(pageInfo.getTotal());
-            info.setSearchAfter(pageInfo.getSearchAfter());
             return info;
         }
     }
--- mall-common/src/main/java/com/github/common/page/Pages.java
+++ mall-common/src/main/java/com/github/common/page/Pages.java
@@ -6,25 +6,6 @@ import com.github.common.util.U;
 
 import java.util.List;
 
-/**
- * <pre>
- * 此实体类在 Controller 和 Service 中用到分页时使用.
- *
- * &#064;Controller --> request 请求中带过来的参数使用 Page 进行接收(如果前端不传, 此处接收则程序会使用默认值)
- * public JsonResult xx(xxx, PageParam page) {
- *     PageReturn pageInfo = xxxService.page(xxx, page);
- *     return success("xxx", (page.isWasMobile() ? pageInfo.getList() : pageInfo));
- * }
- *
- * &#064;Service --> 调用方法使用 Page 进行传递, 返回 PageInfo
- * public PageReturn page(xxx, PageParam page) {
- *     List&lt;XXX> xxxList = xxxMapper.selectPage(Pages.param(page), xxxxx);
- *     return Pages.returnPage(xxxList);
- * }
- *
- * 这么做的目的是分页包只需要在服务端引入即可
- * </pre>
- */
 public final class Pages {
 
     /** 在 service 的实现类中调用 --> 当不想查 select count(*) 时用这个 */
