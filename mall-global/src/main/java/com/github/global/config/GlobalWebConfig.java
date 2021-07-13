a12e0bdba617cdf46027516f233b52e3cde0af13
Jie <zt17947@ztn.com>
2021-07-13 09:23:57 +0800

    fix

--- mall-global/src/main/java/com/github/global/config/GlobalWebConfig.java
+++ mall-global/src/main/java/com/github/global/config/GlobalWebConfig.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.servlet.Filter;
@@ -16,17 +14,16 @@ import javax.servlet.Filter;
 public class GlobalWebConfig {
 
     @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    public FilterRegistrationBean<CorsFilter> corsFilter() {
-        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new CorsFilter());
-        return registrationBean;
+    public FilterRegistrationBean<CharacterEncodingFilter> characterFilter(CharacterEncodingFilter filter) {
+        FilterRegistrationBean<CharacterEncodingFilter> filterBean = new FilterRegistrationBean<>(filter);
+        filterBean.setOrder(Integer.MIN_VALUE);
+        return filterBean;
     }
 
     @Bean
-    public FilterRegistrationBean<CharacterEncodingFilter> characterFilter(CharacterEncodingFilter filter) {
-        FilterRegistrationBean<CharacterEncodingFilter> filterBean = new FilterRegistrationBean<>();
-        filterBean.setFilter(filter);
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter());
+        filterBean.setOrder(Integer.MIN_VALUE + 1);
         return filterBean;
     }
 }
