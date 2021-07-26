af6bbfea17aebcc1639e4835a47b54f7f3cbbc22
Jie <zt17947@ztn.com>
2021-07-26 15:23:05 +0800

    fix

--- mall-global/src/main/java/com/github/global/config/TaskConfig.java
+++ mall-global/src/main/java/com/github/global/config/TaskConfig.java
@@ -28,9 +28,9 @@ public class TaskConfig implements AsyncConfigurer {
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        // 如果 cpu 核心是 8, 则最前面的 16 个异步处理, 后面的 1984 个放进队列, 之后的 48 个(64 - 16)异步处理, 再往后的拒绝
+        // 如果 cpu 核心是 8, 则最前面的 16 个异步处理, 后面的 1984 个放进队列, 之后的 16 个(32 - 16)异步处理, 再往后的拒绝
         executor.setCorePoolSize(U.PROCESSORS << 1);     // 8 * 2 = 16
-        executor.setMaxPoolSize(U.PROCESSORS << 3);      // 8 * 8 = 64
+        executor.setMaxPoolSize(U.PROCESSORS << 2);      // 8 * 4 = 32
         executor.setQueueCapacity((U.PROCESSORS << 8) - (U.PROCESSORS << 3)); // (8 * 256) - (8 * 8) = 1984
         executor.setThreadNamePrefix("task-executor-");  // 线程名字的前缀
         // 见: https://moelholm.com/blog/2017/07/24/spring-43-using-a-taskdecorator-to-copy-mdc-data-to-async-threads
