4798ea1939102d905f24d03a981c2bad159e37b6
Jie <zt17947@ztn.com>
2021-09-14 10:17:37 +0800

    fix

---
+++ mall-common/src/main/java/com/github/common/exception/ParamException.java
@@ -0,0 +1,27 @@
+package com.github.common.exception;
+
+import com.github.common.util.A;
+import lombok.Getter;
+
+import java.util.Map;
+
+/** 自定义参数校验异常 */
+@Getter
+public class ParamException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	private final Map<String, String> validationErrorMap;
+
+	public ParamException(String field, String message) {
+		this.validationErrorMap = A.maps(field, message);
+	}
+
+	public ParamException(Map<String, String> messageMap) {
+		this.validationErrorMap = messageMap;
+	}
+
+	@Override
+	public Throwable fillInStackTrace() {
+		return this;
+	}
+}
