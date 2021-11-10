bb85a34bfd8a24880fd6060de06eb82ef29d342c
Jie <zt17947@ztn.com>
2021-11-10 15:41:37 +0800

    fix

--- mall-global/src/main/java/com/github/global/service/RedissonService.java
+++ mall-global/src/main/java/com/github/global/service/RedissonService.java
@@ -3,6 +3,8 @@ package com.github.global.service;
 import lombok.AllArgsConstructor;
 import org.redisson.api.RMap;
 import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.Codec;
+import org.redisson.client.codec.StringCodec;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,13 +22,15 @@ public class RedissonService {
 
     // https://github.com/redisson/redisson/wiki/11.-Redis-commands-mapping
 
+    private static final Codec USE_CODEC = StringCodec.INSTANCE;
+
     /** @see org.redisson.spring.starter.RedissonAutoConfiguration */
     private final RedissonClient redisson;
 
 
     /** 往 redis 中放值 */
     public <T> void set(String key, T value) {
-        redisson.getBucket(key).set(value);
+        redisson.getBucket(key, USE_CODEC).set(value);
     }
     /** 往 redis 放值, 并设定在什么时间超时 */
     public <T> void set(String key, T value, Date expireTime) {
@@ -39,11 +43,11 @@ public class RedissonService {
     }
     /** 往 redis 放值, 并设定超时时间 */
     public <T> void set(String key, T value, long time, TimeUnit unit) {
-        redisson.getBucket(key).set(value, time, unit);
+        redisson.getBucket(key, USE_CODEC).set(value, time, unit);
     }
 
     public void expire(String key, long time, TimeUnit unit) {
-        redisson.getBucket(key).expire(time, unit);
+        redisson.getBucket(key, USE_CODEC).expire(time, unit);
     }
 
     public long incr(String key) {
@@ -52,11 +56,11 @@ public class RedissonService {
 
     /** 从 redis 中取值 */
     public <T> T get(String key) {
-        return (T) redisson.getBucket(key).get();
+        return (T) redisson.getBucket(key, USE_CODEC).get();
     }
     /** 从 redis 中删值 */
     public void delete(String key) {
-        redisson.getBucket(key).delete();
+        redisson.getBucket(key, USE_CODEC).delete();
     }
 
 
@@ -188,27 +192,27 @@ public class RedissonService {
 
     /** 向 list 写值(从左边入): lpush */
     public <T> void leftPush(String key, T value) {
-        redisson.getDeque(key).push(value);
+        redisson.getDeque(key, USE_CODEC).push(value);
     }
     /** 向 list 写值(从右边入): rpush */
     public <T> void rightPush(String key, T value) {
-        redisson.getDeque(key).add(value);
+        redisson.getDeque(key, USE_CODEC).add(value);
     }
     /** 向 list 取值(从左边出): lpop */
     public <T> T leftPop(String key) {
-        return (T) redisson.getDeque(key).pollFirst();
+        return (T) redisson.getDeque(key, USE_CODEC).pollFirst();
     }
     /** 向 list 取值(从左边出, 并阻塞指定的时间): blpop */
     public <T> T leftPop(String key, int time, TimeUnit unit) throws InterruptedException {
-        return (T) redisson.getBlockingDeque(key).pollFirst(time, unit);
+        return (T) redisson.getBlockingDeque(key, USE_CODEC).pollFirst(time, unit);
     }
     /** 向 list 取值(从右边出): rpop */
     public <T> T rightPop(String key) {
-        return (T) redisson.getDeque(key).pollLast();
+        return (T) redisson.getDeque(key, USE_CODEC).pollLast();
     }
     /** 向 list 取值(从右边出, 并阻塞指定的时间): brpop */
     public <T> T rightPop(String key, int time, TimeUnit unit) throws InterruptedException {
-        return (T) redisson.getBlockingDeque(key).pollLast(time, unit);
+        return (T) redisson.getBlockingDeque(key, USE_CODEC).pollLast(time, unit);
     }
 
 
@@ -216,28 +220,28 @@ public class RedissonService {
 
     /** 获取指定 set 的长度: scard key */
     public long setSize(String key) {
-        return redisson.getSet(key).size();
+        return redisson.getSet(key, USE_CODEC).size();
     }
     /** 将指定的 set 存进 redis 并返回成功条数: sadd key v1 v2 v3 ... */
     public <T> boolean setAdd(String key, Collection<T> set) {
         // return add != null ? add : 0L;
-        return redisson.getSet(key).addAll(set);
+        return redisson.getSet(key, USE_CODEC).addAll(set);
     }
     /** 获取 set: smembers key */
     public <T> Set<T> setGet(String key) {
-        return redisson.getSet(key);
+        return redisson.getSet(key, USE_CODEC);
     }
     /** 从 set 中移除一个值: srem key member */
     public <T> void setRemove(String key, T value) {
-        redisson.getSet(key).remove(value);
+        redisson.getSet(key, USE_CODEC).remove(value);
     }
     /** 从指定的 set 中随机取出一个值: spop key */
     public <T> T setPop(String key) {
-        return (T) redisson.getSet(key).removeRandom();
+        return (T) redisson.getSet(key, USE_CODEC).removeRandom();
     }
     /** 从指定的 set 中随机取出一些值: spop key count */
     public <T> Set<T> setPop(String key, int count) {
-        return (Set<T>) redisson.getSet(key).removeRandom(count);
+        return (Set<T>) redisson.getSet(key, USE_CODEC).removeRandom(count);
     }
 
 
@@ -245,35 +249,35 @@ public class RedissonService {
 
     /** 写 hash: hmset key field value [field value ...] */
     public <T> void hashPutAll(String key, Map<String, T> hashMap) {
-        redisson.getMap(key).putAll(hashMap);
+        redisson.getMap(key, USE_CODEC).putAll(hashMap);
     }
     /** 写一个 hash: hset key field value */
     public <T> void hashPut(String key, String hashKey, T hashValue) {
-        redisson.getMap(key).put(hashKey, hashValue);
+        redisson.getMap(key, USE_CODEC).put(hashKey, hashValue);
     }
     /** 写一个 hash, 只有 hash 中没有这个 key 才能写成功, 有了就不写: hsetnx key field value */
     public <T> void hashPutIfAbsent(String key, String hashKey, T hashValue) {
-        redisson.getMap(key).putIfAbsent(hashKey, hashValue);
+        redisson.getMap(key, USE_CODEC).putIfAbsent(hashKey, hashValue);
     }
     /** 获取一个 hash 的长度: hlen key */
     public int hashSize(String key) {
-        return redisson.getMap(key).size();
+        return redisson.getMap(key, USE_CODEC).size();
     }
     /** 获取一个 hash 的值: hgetall key */
     public <T> Map<String, T> hashGetAll(String key) {
-        RMap<String, T> map = redisson.getMap(key);
+        RMap<String, T> map = redisson.getMap(key, USE_CODEC);
         return map.readAllMap();
     }
     /** 获取一个 hash 中指定 key 的值: hget key field */
     public <T> T hashGet(String key, String hashKey) {
-        return (T) redisson.getMap(key).get(hashKey);
+        return (T) redisson.getMap(key, USE_CODEC).get(hashKey);
     }
     /** 给 hash 中指定的 key 的值累加 1: hincrby key field 1 */
     public void hashIncr(String key, String hashKey) {
-        redisson.getMap(key).addAndGet(hashKey, 1);
+        redisson.getMap(key, USE_CODEC).addAndGet(hashKey, 1);
     }
     /** 从 hash 中移除指定的 key: hdel key field */
     public void hashRemove(String key, String hashKey) {
-        redisson.getMap(key).fastRemove(hashKey);
+        redisson.getMap(key, USE_CODEC).fastRemove(hashKey);
     }
 }
