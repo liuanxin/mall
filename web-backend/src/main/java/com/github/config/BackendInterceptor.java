d0be289e80cc64aba952778c7018b764411ea243
Jie <zt17947@ztn.com>
2021-12-08 15:50:32 +0800

    fix

--- web-backend/src/main/java/com/github/config/BackendInterceptor.java
+++ web-backend/src/main/java/com/github/config/BackendInterceptor.java
@@ -12,14 +12,12 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.lang.annotation.Annotation;
 
 @SuppressWarnings("NullableProblems")
 public class BackendInterceptor implements HandlerInterceptor {
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
-                             Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler) {
         bindParam();
         // RequestUtils.handleLocal("lang");
         checkLoginAndPermission(handler);
@@ -27,16 +25,14 @@ public class BackendInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response,
-                           Object handler, ModelAndView modelAndView) throws Exception {
+    public void postHandle(HttpServletRequest req, HttpServletResponse res, Object handler, ModelAndView mv) {
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
-                                Object handler, Exception ex) throws Exception {
-        if (ex != null) {
+    public void afterCompletion(HttpServletRequest req, HttpServletResponse res, Object handler, Exception e) {
+        if (e != null) {
             if (LogUtil.ROOT_LOG.isErrorEnabled()) {
-                LogUtil.ROOT_LOG.error("request was over, but have exception: " + ex.getMessage());
+                LogUtil.ROOT_LOG.error("request was over, but have exception: " + e.getMessage());
             }
         }
         unbindParam();
@@ -58,15 +54,14 @@ public class BackendInterceptor implements HandlerInterceptor {
             return;
         }
 
-        NeedLogin needLogin = getAnnotation((HandlerMethod) handler, NeedLogin.class);
+        HandlerMethod method = (HandlerMethod) handler;
+        NeedLogin needLogin = method.getMethodAnnotation(NeedLogin.class);
+        if (needLogin == null) {
+            needLogin = AnnotationUtils.findAnnotation(method.getBeanType(), NeedLogin.class);
+        }
         // 标注了 @NeedLogin 且 flag 为 true(默认就是 true)则表示当前请求需要登录
         if (needLogin != null && needLogin.value()) {
             BackendSessionUtil.checkLogin();
         }
-    }
-    private <T extends Annotation> T getAnnotation(HandlerMethod handlerMethod, Class<T> clazz) {
-        // 先找方法上的注解, 没有再找类上的注解
-        T annotation = handlerMethod.getMethodAnnotation(clazz);
-        return annotation == null ? AnnotationUtils.findAnnotation(handlerMethod.getBeanType(), clazz) : annotation;
     }
 }
