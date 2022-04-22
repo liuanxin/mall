36da19bbdd372c02d2bf1c9d3986c634cff815ac
Jie <zt17947@ztn.com>
2022-04-22 10:20:16 +0800

    fix

--- mall-common/src/main/java/com/github/common/mvc/LanguageFilter.java
+++ mall-common/src/main/java/com/github/common/mvc/LanguageFilter.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.support.RequestContextUtils;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Locale;
@@ -15,8 +16,8 @@ import java.util.Locale;
 /**
  * 基于下面的优先级依次获取语言
  *
- * 1. 参数里的 langParamName
- * 2. 头里的 Accept-Language
+ * 1. 参数里的 langParamName (默认是 lang)
+ * 2. 头里的 langParamName (默认是 lang)
  * 3. request.getLocale()
  * 4. 简体中文
  *
@@ -37,37 +38,52 @@ public class LanguageFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletRequestWrapper wrapper = new HttpServletRequestWrapper(request) {
+            @Override
+            public Locale getLocale() {
+                String lanParam = U.defaultIfBlank(languageParam, "lang");
+                String lan = request.getParameter(lanParam);
+                if (U.isBlank(lan)) {
+                    lan = request.getHeader(lanParam);
+                }
 
-        String lanParam = U.defaultIfBlank(languageParam, "lang");
-        String lan = request.getParameter(lanParam);
-        if (U.isBlank(lan)) {
-            lan = request.getHeader("Accept-Language");
-        }
+                Locale locale = null;
+                try {
+                    if (U.isNotBlank(lan)) {
+                        locale = Locale.forLanguageTag(lan);
+                    }
+                } catch (Exception e) {
+                    if (LogUtil.ROOT_LOG.isErrorEnabled()) {
+                        LogUtil.ROOT_LOG.error("parse Local exception", e);
+                    }
+                }
 
-        Locale locale = null;
-        try {
-            if (U.isNotBlank(lan)) {
-                locale = Locale.forLanguageTag(lan);
+                if (U.isNull(locale) || (U.isBlank(locale.getLanguage()) && U.isBlank(locale.getCountry()))) {
+                    locale = request.getLocale();
+                }
+                if (U.isNull(locale) || (U.isBlank(locale.getLanguage()) && U.isBlank(locale.getCountry()))) {
+                    locale = Locale.SIMPLIFIED_CHINESE;
+                }
+                if (U.isNotNull(locale)) {
+                    // 中文就使用简体中文, 英文就使用美式英文(如果有具体的 zh_CN、zh_TW、en_GB、en_US、en_CA 就不应用这样处理)
+                    String language = locale.getLanguage();
+                    if ("zh".equalsIgnoreCase(language)) {
+                        locale = Locale.SIMPLIFIED_CHINESE;
+                    } else if ("en".equalsIgnoreCase(language)) {
+                        locale = Locale.US;
+                    }
+                }
+                return locale;
             }
-        } catch (Exception e) {
-            if (LogUtil.ROOT_LOG.isErrorEnabled()) {
-                LogUtil.ROOT_LOG.error("parse Local exception", e);
-            }
-        }
-        if (U.isNull(locale) || (U.isBlank(locale.getLanguage()) && U.isBlank(locale.getCountry()))) {
-            locale = request.getLocale();
-        }
-        if (U.isNull(locale) || (U.isBlank(locale.getLanguage()) && U.isBlank(locale.getCountry()))) {
-            locale = Locale.SIMPLIFIED_CHINESE;
-        }
+        };
 
+        Locale locale = wrapper.getLocale();
         LocaleContextHolder.setLocale(locale);
         LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
         if (U.isNotNull(localeResolver)) {
             localeResolver.setLocale(request, (HttpServletResponse) res, locale);
         }
-
-        chain.doFilter(req, res);
+        chain.doFilter(wrapper, res);
     }
 
     @Override
