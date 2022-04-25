efd45baf2056ba54c839743c5eef966e67980066
Jie <zt17947@ztn.com>
2022-04-25 19:11:06 +0800

    fix

--- mall-mq/src/main/java/com/github/mq/config/RabbitConfig.java
+++ mall-mq/src/main/java/com/github/mq/config/RabbitConfig.java
@@ -3,7 +3,6 @@ package com.github.mq.config;
 import com.github.common.util.LogUtil;
 import com.github.common.util.U;
 import com.github.mq.constant.MqInfo;
-import com.google.common.collect.Maps;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -12,6 +11,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Configuration
@@ -26,26 +26,26 @@ public class RabbitConfig {
     public RabbitAdmin rabbitAdmin() {
         RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
 
-        Map<String, Exchange> exchangeMap = Maps.newHashMap();
-        Map<String, Queue> queueMap = Maps.newHashMap();
-        Map<String, Binding> bindingMap = Maps.newHashMap();
+        Map<String, Exchange> exchangeMap = new LinkedHashMap<>();
+        Map<String, Queue> queueMap = new LinkedHashMap<>();
+        Map<String, Binding> bindingMap = new LinkedHashMap<>();
         for (MqInfo mqInfo : MqInfo.values()) {
+            String exchangeType = mqInfo.getExchangeType();
             String exchangeName = mqInfo.getExchangeName();
             String routingKey = mqInfo.getRoutingKey();
-            String exchangeType = mqInfo.getExchangeType();
             String queueName = mqInfo.getQueueName();
-            String bindingName = String.format("%s(%s) --> %s", exchangeName, routingKey, queueName);
+            String bindingName = String.format("(%s -- %s --> %s)", exchangeName, routingKey, queueName);
 
             Exchange exchange = exchangeMap.get(exchangeName);
             if (U.isNull(exchange)) {
-                // 默认持久化(durable 是 true), 不自动删除(autoDelete 是 false)
+                // 持久化(durable 是 true), 不自动删除(autoDelete 是 false)
                 exchange = new ExchangeBuilder(exchangeName, exchangeType).build();
                 exchangeMap.put(exchangeName, exchange);
             }
 
             Queue queue = queueMap.get(queueName);
             if (U.isNull(queue)) {
-                // 持久化, 默认不自动删除(autoDelete 是 false)
+                // 持久化(durable 是 true), 不自动删除(autoDelete 是 false)
                 queue = QueueBuilder.durable(queueName).withArguments(mqInfo.getArgs()).build();
                 queueMap.put(queueName, queue);
             }
@@ -59,21 +59,21 @@ public class RabbitConfig {
             rabbitAdmin.declareExchange(exchange);
         }
         if (LogUtil.ROOT_LOG.isInfoEnabled()) {
-            LogUtil.ROOT_LOG.info("declare RabbitMQ exchange({} 个: {})", exchangeMap.size(), exchangeMap.keySet());
+            LogUtil.ROOT_LOG.info("declare RabbitMQ exchange({} : {})", exchangeMap.size(), exchangeMap.keySet());
         }
 
         for (Queue queue : queueMap.values()) {
             rabbitAdmin.declareQueue(queue);
         }
         if (LogUtil.ROOT_LOG.isInfoEnabled()) {
-            LogUtil.ROOT_LOG.info("declare RabbitMQ queue({} 个: {})", queueMap.size(), queueMap.keySet());
+            LogUtil.ROOT_LOG.info("declare RabbitMQ queue({} : {})", queueMap.size(), queueMap.keySet());
         }
 
         for (Binding binding : bindingMap.values()) {
             rabbitAdmin.declareBinding(binding);
         }
         if (LogUtil.ROOT_LOG.isInfoEnabled()) {
-            LogUtil.ROOT_LOG.info("declare RabbitMQ binding({} 个: {})", bindingMap.size(), bindingMap.keySet());
+            LogUtil.ROOT_LOG.info("declare RabbitMQ binding({} : {})", bindingMap.size(), bindingMap.keySet());
         }
         return rabbitAdmin;
     }
