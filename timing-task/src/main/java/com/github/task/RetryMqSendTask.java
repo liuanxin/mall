5cd6e4e7dcdc208a8c4a6c6ae21a0fa246a5e32d
Jie <zt17947@ztn.com>
2022-06-14 14:35:02 +0800

    fix

--- timing-task/src/main/java/com/github/task/RetryMqSendTask.java
+++ timing-task/src/main/java/com/github/task/RetryMqSendTask.java
@@ -21,9 +21,13 @@ import java.util.List;
 @SuppressWarnings("NullableProblems")
 public class RetryMqSendTask implements SchedulingConfigurer {
 
-    /** 当前定时任务的业务说明 */
+    /**
+     * 当前定时任务的业务说明
+     */
     private static final String BUSINESS_DESC = "重试 mq 发送";
-    /** 当前任务的默认表达式 */
+    /**
+     * 当前任务的默认表达式
+     */
     private static final String CRON = "13 0/5 * * * *";
 
     @Value("mq.singleRetryCount:20")
@@ -41,51 +45,66 @@ public class RetryMqSendTask implements SchedulingConfigurer {
         DynamicCronUtil.runTask(schedule, BUSINESS_DESC, CRON, this::handlerBusiness);
     }
 
-    /** 操作具体的业务 */
+    /**
+     * 操作具体的业务
+     */
     public boolean handlerBusiness() {
         for (;;) {
             List<MqSend> mqSendList = mqSendService.queryRetryMsg(maxRetryCount, mqSingleRetryCount);
             if (A.isNotEmpty(mqSendList)) {
                 return true;
             }
-            retrySend(mqSendList);
+            for (MqSend mqSend : mqSendList) {
+                retrySend(mqSend);
+            }
             if (mqSendList.size() < mqSingleRetryCount) {
                 return true;
             }
         }
     }
 
-    private void retrySend(List<MqSend> mqSendList) {
-        for (MqSend mqSend : mqSendList) {
-            String msgId = mqSend.getMsgId();
-            if (redissonService.tryLock(msgId)) {
-                try {
-                    MqInfo mqInfo = MqInfo.from(mqSend.getBusinessType());
-                    if (U.isNotNull(mqInfo)) {
-                        if (LogUtil.ROOT_LOG.isInfoEnabled()) {
-                            LogUtil.ROOT_LOG.info("{} --> {}", BUSINESS_DESC, msgId);
-                        }
-                        try {
-                            mqSenderHandler.doProvide(mqInfo, null, mqSend.getMsg());
-                            if (LogUtil.ROOT_LOG.isInfoEnabled()) {
-                                LogUtil.ROOT_LOG.info("{} --> {} 成功", BUSINESS_DESC, msgId);
-                            }
-                        } catch (Exception e) {
-                            if (LogUtil.ROOT_LOG.isErrorEnabled()) {
-                                LogUtil.ROOT_LOG.error("{} --> {} 异常", BUSINESS_DESC, msgId, e);
-                            }
-                        }
-                    } else {
-                        // 没有 mq-info 的数据直接置为成功, 无法重试
-                        MqSend update = new MqSend();
-                        update.setId(mqSend.getId());
-                        update.setStatus(2);
-                        mqSendService.updateById(update);
-                    }
-                } finally {
-                    redissonService.unlock(msgId);
-                }
-            }
+    private void retrySend(MqSend mqSend) {
+        MqInfo mqInfo = MqInfo.from(mqSend.getBusinessType());
+        if (U.isNull(mqInfo)) {
+            // 没有 mq-info 的数据直接置为成功, 无法重试
+            MqSend update = new MqSend();
+            update.setId(mqSend.getId());
+            update.setStatus(2);
+            update.setRemark(mqSend.getRemark() + ";;没有这个 business_type 的场景");
+            mqSendService.updateById(update);
+            return;
         }
+
+        String msgId = mqSend.getMsgId();
+        if (redissonService.tryLock(msgId)) {
+            try {
+                if (LogUtil.ROOT_LOG.isInfoEnabled()) {
+                    LogUtil.ROOT_LOG.info("{} --> {}", BUSINESS_DESC, msgId);
+                }
+                try {
+                    mqSenderHandler.doProvide(mqInfo, null, mqSend.getMsg());
+                    if (LogUtil.ROOT_LOG.isInfoEnabled()) {
+                        LogUtil.ROOT_LOG.info("{} --> {} 成功", BUSINESS_DESC, msgId);
+                    }
+                } catch (Exception e) {
+                    if (LogUtil.ROOT_LOG.isErrorEnabled()) {
+                        LogUtil.ROOT_LOG.error("{} --> {} 异常", BUSINESS_DESC, msgId, e);
+                    }
+                }
+            } finally {
+                redissonService.unlock(msgId);
+            }
+            return;
+        }
+
+        if (LogUtil.ROOT_LOG.isInfoEnabled()) {
+            LogUtil.ROOT_LOG.info("{} --> {} 正在运行", BUSINESS_DESC, msgId);
+        }
+        // msgId 的数据只需要有一条在处理, 当前数据直接置为成功, 无需重试
+        MqSend update = new MqSend();
+        update.setId(mqSend.getId());
+        update.setStatus(2);
+        update.setRemark(mqSend.getRemark() + ";;同 msg_id 的任务正在执行");
+        mqSendService.updateById(update);
     }
 }
