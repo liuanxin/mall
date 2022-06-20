adf9b24b6761eb50dd89f16613f0bf97abb4d63b
Jie <zt17947@ztn.com>
2022-06-20 16:54:59 +0800

    fix

--- mall-common/src/main/java/com/github/common/mvc/CorsFilter.java
+++ mall-common/src/main/java/com/github/common/mvc/CorsFilter.java
@@ -18,12 +18,14 @@ public class CorsFilter implements Filter {
     private static final String CREDENTIALS = "true";
     private static final String METHODS = A.toStr(Const.SUPPORT_METHODS);
     private static final String HEADERS = A.toStr(Const.ALLOW_HEADERS);
+    private static final String MAX_AGE = "600";
 
     /** @see org.springframework.http.HttpHeaders */
     private static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
     private static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
     private static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
     private static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    private static final String ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age";
 
     // /** for ie: https://www.lovelucy.info/ie-accept-third-party-cookie.html */
     // private static final String P3P = "P3P";
@@ -52,16 +54,31 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
 
         String origin = request.getHeader(ORIGIN);
-        if (U.isNotBlank(origin)) {
+        if (U.isBlank(origin)) {
+            if (hasOptions(request)) {
+                response.setStatus(HttpServletResponse.SC_OK);
+                return;
+            }
+        } else {
             String domain = getDomain(request);
             // 头里面带过来的 origin 跟请求的不一样才需要设置 cors
             if (U.isNotEquals(domain, origin)) {
                 // 配置项为空则设置 cors
                 if (A.isEmpty(allowOriginSet)) {
+                    if (hasOptions(request)) {
+                        response.setStatus(HttpServletResponse.SC_OK);
+                        return;
+                    }
+
                     setOrigin(response, origin);
                 } else {
                     // 头里面带过来的 origin 是在配置项里面才设置 cors
                     if (allowOriginSet.contains(origin)) {
+                        if (hasOptions(request)) {
+                            response.setStatus(HttpServletResponse.SC_OK);
+                            return;
+                        }
+
                         setOrigin(response, origin);
                     }
                 }
@@ -69,11 +86,15 @@ public class CorsFilter implements Filter {
         }
         chain.doFilter(request, response);
     }
+    private boolean hasOptions(HttpServletRequest request) {
+        return "OPTIONS".equalsIgnoreCase(request.getMethod());
+    }
     private void setOrigin(HttpServletResponse response, String origin) {
         response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, origin);
         response.setHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, CREDENTIALS);
         response.setHeader(ACCESS_CONTROL_ALLOW_METHODS, METHODS);
         response.setHeader(ACCESS_CONTROL_ALLOW_HEADERS, HEADERS);
+        response.setHeader(ACCESS_CONTROL_MAX_AGE, MAX_AGE);
         /*
         if (RequestUtils.isIeRequest() && U.isEmpty(response.getHeader(P3P))) {
             response.addHeader(P3P, IEP3P);
