4f72260463844f2fd3334a3ad74af43267fc9a4f
Jie <zt17947@ztn.com>
2022-06-21 10:29:29 +0800

    fix

--- mall-global/src/main/java/com/github/global/config/TaskConfig.java
+++ mall-global/src/main/java/com/github/global/config/TaskConfig.java
@@ -4,6 +4,7 @@ import com.github.common.util.AsyncUtil;
 import com.github.common.util.U;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -23,6 +24,15 @@ import java.util.concurrent.Executor;
 @EnableAsync
 public class TaskConfig implements AsyncConfigurer {
 
+    @Value("asyncTask.corePoolSize:0")
+    private int corePoolSize;
+
+    @Value("task.maxPoolSize:0")
+    private int maxPoolSize;
+
+    @Value("task.queueCapacity:0")
+    private int queueCapacity;
+
     /**
      * @see org.springframework.boot.autoconfigure.task.TaskExecutionProperties
      * @see org.springframework.aop.interceptor.AsyncExecutionInterceptor#determineAsyncExecutor
@@ -40,9 +50,9 @@ public class TaskConfig implements AsyncConfigurer {
         // 如果 cpu 核心是  64, 则最前面的  63 个异步处理, 接下来的  4096 个放进队列, 之后的 3 个(  64 + 2 -  63)异步处理, 再往后的拒绝
         // 如果 cpu 核心是 128, 则最前面的 127 个异步处理, 接下来的  8192 个放进队列, 之后的 3 个( 128 + 2 - 127)异步处理, 再往后的拒绝
         // 如果 cpu 核心是 256, 则最前面的 255 个异步处理, 接下来的 16384 个放进队列, 之后的 3 个( 256 + 2 - 255)异步处理, 再往后的拒绝
-        executor.setCorePoolSize(Math.max(U.PROCESSORS - 1, 1));
-        executor.setMaxPoolSize(U.PROCESSORS + 2);
-        executor.setQueueCapacity(U.PROCESSORS << 6);
+        executor.setCorePoolSize(corePoolSize > 0 ? corePoolSize : Math.max(U.PROCESSORS - 1, 1));
+        executor.setMaxPoolSize(maxPoolSize > 0 ? maxPoolSize : (U.PROCESSORS + 2));
+        executor.setQueueCapacity(queueCapacity > 0 ? queueCapacity : (U.PROCESSORS << 6));
         executor.setThreadNamePrefix("task-executor-");
         // 见: https://moelholm.com/blog/2017/07/24/spring-43-using-a-taskdecorator-to-copy-mdc-data-to-async-threads
         executor.setTaskDecorator(AsyncUtil::wrapRunContext);
