a8177489a8a6a09f290c4b9e5dc024d60de2264c
Jie <zt17947@ztn.com>
2022-08-24 16:46:41 +0800

    fix rebuild

--- mall-common/src/main/java/com/github/common/export/poi/ExportColumnHandler.java
+++ mall-common/src/main/java/com/github/common/export/poi/ExportColumnHandler.java
@@ -9,7 +9,7 @@ import java.util.LinkedHashMap;
 public class ExportColumnHandler {
 
     /** 从类上收集导出的标题(在字段上标的 &#064;ExportColumn 注解) */
-    public static LinkedHashMap<String, String> collectTitle(Class clazz) {
+    public static LinkedHashMap<String, String> collectTitle(Class<?> clazz) {
         LinkedHashMap<String, String> titleMap = new LinkedHashMap<>();
         if (U.isNotNull(clazz)) {
             Field[] fields = clazz.getDeclaredFields();
