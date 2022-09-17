b3b660ef52501d967cf60ba8589cca66e6a357ae
liuanxin <liu.anxin13@gmail.com>
2022-09-17 14:02:10 +0800

    fix

--- timing-task/src/main/java/com/github/TimingTaskApplication.java
+++ timing-task/src/main/java/com/github/TimingTaskApplication.java
@@ -16,9 +16,12 @@ public class TimingTaskApplication {
     public static void main(String[] args) {
         long ms = System.currentTimeMillis();
         ApplicationContext ctx = new SpringApplicationBuilder(TimingTaskApplication.class).web(WebApplicationType.NONE).run(args);
-        if (LogUtil.ROOT_LOG.isDebugEnabled()) {
-            LogUtil.ROOT_LOG.info("run success, current profile({}), use time({})",
-                    A.toStr(ctx.getEnvironment().getActiveProfiles()), DateUtil.toHuman(System.currentTimeMillis() - ms));
+        if (LogUtil.ROOT_LOG.isInfoEnabled()) {
+            String profile = A.toStr(ctx.getEnvironment().getActiveProfiles());
+            String appName = ctx.getEnvironment().getProperty("spring.application.name");
+            String port = ctx.getEnvironment().getProperty("server.port");
+            String time = DateUtil.toHuman(System.currentTimeMillis() - ms);
+            LogUtil.ROOT_LOG.info("run({}) profile({}), port({}), use time({})", appName, profile, port, time);
         }
     }
 }
