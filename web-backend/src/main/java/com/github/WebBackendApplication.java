2b7fb93536120a3ac4493b739f34e53db77c29a5
liuanxin <liu.anxin13@gmail.com>
2022-09-24 14:07:33 +0800

    fix

--- web-backend/src/main/java/com/github/WebBackendApplication.java
+++ web-backend/src/main/java/com/github/WebBackendApplication.java
@@ -3,10 +3,11 @@ package com.github;
 import com.github.common.date.DateUtil;
 import com.github.common.util.A;
 import com.github.common.util.LogUtil;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class WebBackendApplication extends SpringBootServletInitializer {
@@ -18,7 +19,7 @@ public class WebBackendApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         long ms = System.currentTimeMillis();
-        ApplicationContext ctx = new SpringApplicationBuilder().sources(WebBackendApplication.class).run(args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(WebBackendApplication.class, args);
         if (LogUtil.ROOT_LOG.isInfoEnabled()) {
             String profile = A.toStr(ctx.getEnvironment().getActiveProfiles());
             String appName = ctx.getEnvironment().getProperty("spring.application.name");
