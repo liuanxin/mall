a7d616d6ae21e18f3ed4b43058c8058a0acd1e0e
Jie <liu_anxin@163.com>
2022-10-09 17:44:43 +0800

    fix rebuild

--- mall-global/src/main/java/com/github/global/query/model/SupplierSerialize.java
+++
@@ -1,7 +0,0 @@
-package com.github.global.query.model;
-
-import java.io.Serializable;
-import java.util.function.Supplier;
-
-public interface SupplierSerialize<T> extends Supplier<T>, Serializable {
-}
