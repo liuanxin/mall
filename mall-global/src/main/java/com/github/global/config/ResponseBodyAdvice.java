9211f18d3bfc8a389172fd1c839e0605c843b348
TY <liu.anxin13@gmail.com>
2023-03-30 09:36:25 +0800

    fix

--- mall-global/src/main/java/com/github/global/config/ResponseBodyAdvice.java
+++ mall-global/src/main/java/com/github/global/config/ResponseBodyAdvice.java
@@ -5,7 +5,6 @@ import com.github.common.util.LogUtil;
 import com.github.common.util.RequestUtil;
 import com.github.common.util.U;
 import com.github.global.constant.GlobalConst;
-import javassist.ClassPool;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.core.MethodParameter;
@@ -26,8 +25,6 @@ import java.lang.reflect.Method;
 @ConditionalOnClass({ ResponseBody.class })
 @ControllerAdvice(annotations = { Controller.class, RestController.class })
 public class ResponseBodyAdvice extends AbstractMappingJacksonResponseBodyAdvice {
-
-    private static final ClassPool CLASS_POOL = new ClassPool(ClassPool.getDefault());
 
     private final GlobalLogHandler logHandler;
 
@@ -54,23 +51,7 @@ public class ResponseBodyAdvice extends AbstractMappingJacksonResponseBodyAdvice
             String methodName = U.isNotNull(method) ? method.getName() : U.EMPTY;
             if (U.isNotBlank(methodName)) {
                 sbd.append("#").append(methodName);
-                try {
-                    String classInFile = U.getClassInFile(clazz);
-                    if (U.isNotBlank(classInFile)) {
-                        CLASS_POOL.appendClassPath(classInFile);
-                        int line = CLASS_POOL.get(className).getDeclaredMethod(methodName).getMethodInfo().getLineNumber(0);
-                        if (line > 1) {
-                            sbd.append("(").append(clazz.getSimpleName()).append(".java:").append(line - 1).append(")");
-                        }
-                        sbd.append(" in (").append(classInFile).append(")");
-                    }
-                } catch (Exception e) {
-                    if (LogUtil.ROOT_LOG.isDebugEnabled()) {
-                        LogUtil.ROOT_LOG.debug("get {}#{} line-number exception", className, methodName, e);
-                    }
-                }
             }
-
             sbd.append(" return(").append(logHandler.toJson(bodyContainer.getValue())).append(")");
             LogUtil.ROOT_LOG.info(sbd.toString());
         }
