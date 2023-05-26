626acd38d6d15c6b2cc8d34ea0f7d32073efed11
TY <liu.anxin13@gmail.com>
2023-05-26 14:50:30 +0800

    parse user-agent

---
+++ mall-common/src/main/java/com/github/common/util/UserAgentUtil.java
@@ -0,0 +1,55 @@
+package com.github.common.util;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+
+public class UserAgentUtil {
+
+    /** https://cdnjs.com/libraries/UAParser.js */
+    private static final String UA_PARSE_JS_file = "ua-parser.min.js";
+
+    private static final String PARSE_METHOD = "parseInfo";
+
+    /** https://stackoverflow.com/questions/71481562/use-javascript-scripting-engine-in-java-17 */
+    private static final Invocable SCRIPT_ENGINE;
+    static {
+        System.setProperty("polyglot.engine.WarnInterpreterOnly", "false");
+        ScriptEngine engine = null;
+        try {
+            engine = new ScriptEngineManager().getEngineByName("js");
+            URL url = UserAgentUtil.class.getClassLoader().getResource(UA_PARSE_JS_file);
+            if (U.isNotNull(url)) {
+                String uaParserJs = Files.readString(new File(url.getPath()).toPath());
+                String parser = uaParserJs.replace("(?=lg)?", "(?=lg)");
+                String js = ";function " + PARSE_METHOD + "(ua){return JSON.stringify(new UAParser(ua).getResult());}";
+                engine.eval(parser + js);
+            }
+        } catch (IOException e) {
+            if (LogUtil.ROOT_LOG.isErrorEnabled()) {
+                LogUtil.ROOT_LOG.error("file read exception", e);
+            }
+        } catch (ScriptException e) {
+            if (LogUtil.ROOT_LOG.isErrorEnabled()) {
+                LogUtil.ROOT_LOG.error("js file eval exception", e);
+            }
+        }
+        SCRIPT_ENGINE = U.isNull (engine) ? null : (Invocable) engine;
+    }
+
+    public static String parse(String ua) {
+        try {
+            return U.isNull(SCRIPT_ENGINE) ? null : U.toStr(SCRIPT_ENGINE.invokeFunction(PARSE_METHOD, ua));
+        } catch (Exception e) {
+            if (LogUtil.ROOT_LOG.isErrorEnabled()) {
+                LogUtil.ROOT_LOG.error("parse ua({}) exception", ua, e);
+            }
+            return null;
+        }
+    }
+}
---
+++ mall-common/src/test/java/com/github/common/util/UserAgentTest.java
@@ -0,0 +1,24 @@
+package com.github.common.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UserAgentTest {
+
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList(
+                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1",
+                "Mozilla/5.0 (Linux; Android 10; ELS-AN00; HMSCore 5.1.0.309) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 HuaweiBrowser/11.0.6.302 Mobile Safari/537.36",
+                "Dalvik/2.1.0 (Linux; U; Android 10; ELS-AN00 Build/HUAWEIELS-AN00)",
+
+                "Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Weibo(iPhone12,1__weibo__11.8.2__iphone__os14.4.2)",
+                "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Weibo(iPhone11,6__weibo__13.4.3__iphone__os15.4.1)",
+                "Mozilla/5.0 (Linux; Android 11; PDAM10 Build/RKQ1.200903.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 MobileSafari/537.36",
+                "Mozilla/5.0 (Linux; Android 10; V2057A Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/107.0.5304.141 Mobile Safari/537.36 XWEB/5075 MMWEBSDK/20230405 MMWEBID/3511 MicroMessenger/8.0.35.2360(0x2800235B) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 MiniProgramEnv/android"
+        );
+        for (String str : list) {
+            System.out.println("\n----------\n");
+            System.out.println(UserAgentUtil.parse(str));
+        }
+    }
+}
