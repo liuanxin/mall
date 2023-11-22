1a7a9eb3e10c21f018c160127dfec4ab15cfb1c4
TY <liu.anxin13@gmail.com>
2023-11-22 15:16:37 +0800

    fix rebuild

---
+++ mall-common/src/main/java/com/github/common/Function.java
@@ -0,0 +1,23 @@
+package com.github.common;
+
+/**
+ * <pre>
+ * 无入参 + 无返回 用 {@link Empty}
+ *
+ * 单入参 + 无返回 用 {@link java.util.function.Consumer}
+ * 单入参 + 有返回 用 {@link java.util.function.Function}
+ * 单入参 + 返回 boolean 用 {@link java.util.function.Predicate}
+ * 无入参 + 有返回 用 {@link java.util.function.Supplier}
+ *
+ * 两个入参 + 无返回 用 {@link java.util.function.BiConsumer}
+ * 两个入参 + 有返回 用 {@link java.util.function.BiFunction}
+ * 两个入参 + 返回 boolean 用 {@link java.util.function.BiPredicate}
+ * </pre>
+ */
+public class Function {
+
+    /** 无入参 + 无返回 */
+    public interface Empty {
+        void run();
+    }
+}
