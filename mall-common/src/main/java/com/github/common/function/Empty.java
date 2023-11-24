1873767b07cb23343e82912900fee0d37e02aa3b
TY <liu.anxin13@gmail.com>
2023-11-24 10:36:47 +0800

    fix rebuild

--- mall-common/src/main/java/com/github/common/function/Empty.java
+++ mall-common/src/main/java/com/github/common/function/Empty.java
@@ -2,16 +2,15 @@ package com.github.common.function;
 
 /**
  * <pre>
- * 无入参 + 无返回 用 {@link Empty}
+ * {@link Empty} 无入参 + 无返回
  *
- * 单入参 + 无返回 用 {@link java.util.function.Consumer}
- * 单入参 + 有返回 用 {@link java.util.function.Function}
- * 单入参 + 返回 boolean 用 {@link java.util.function.Predicate}
- * 无入参 + 有返回 用 {@link java.util.function.Supplier}
- *
- * 两个入参 + 无返回 用 {@link java.util.function.BiConsumer}
- * 两个入参 + 有返回 用 {@link java.util.function.BiFunction}
- * 两个入参 + 返回 boolean 用 {@link java.util.function.BiPredicate}
+ * {@link java.util.function.Supplier}  无入参 + 有返回
+ * {@link java.util.function.Consumer}  单入参 + 无返回
+ * {@link java.util.function.Function}  单入参 + 有返回
+ * {@link java.util.function.Predicate} 单入参 + 返回 boolean
+ * {@link java.util.function.BiConsumer}  两个入参 + 无返回
+ * {@link java.util.function.BiFunction}  两个入参 + 有返回
+ * {@link java.util.function.BiPredicate} 两个入参 + 返回 boolean
  * </pre>
  */
 public interface Empty {
