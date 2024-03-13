50423697a514ca17562792366b4a74dfc2929244
TY <liu.anxin13@gmail.com>
2024-03-13 15:06:14 +0800

    fix rebuild

--- mall-global/src/main/java/com/github/global/constant/RedisConst.java
+++
@@ -1,8 +0,0 @@
-package com.github.global.constant;
-
-public final class RedisConst {
-
-    public static String exampleKey(String action, String param) {
-        return String.format("%s-%s:%s", action, "example", param);
-    }
-}
