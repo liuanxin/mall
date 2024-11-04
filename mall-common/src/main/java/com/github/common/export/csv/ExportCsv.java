3000573c3be4ced62bea7087a8bae6c7b0c12172
TY <liu.anxin13@gmail.com>
2024-11-04 10:33:42 +0800

    fix rebuild

--- mall-common/src/main/java/com/github/common/export/csv/ExportCsv.java
+++ mall-common/src/main/java/com/github/common/export/csv/ExportCsv.java
@@ -7,10 +7,12 @@ import java.util.*;
 
 public class ExportCsv {
 
-    // 见: https://zh.wikipedia.org/wiki/%E9%80%97%E5%8F%B7%E5%88%86%E9%9A%94%E5%80%BC
-    // 用英文逗号(,)隔开列, 用换行(\n)隔开行
-    //   内容中包含了双引号则替换成两个双引号, 内容中包含了逗号、空格或双引号的则用双引号包裹
-    //   为避免处理换行时出问题: 内容中包含了换行的替换成空格
+    /*
+    见: https://zh.wikipedia.org/wiki/%E9%80%97%E5%8F%B7%E5%88%86%E9%9A%94%E5%80%BC
+    用英文逗号(,)隔开列, 用换行(\n)隔开行
+    内容中包含了双引号则替换成两个双引号, 内容中包含了逗号、空格或双引号的则用双引号包裹
+    为避免处理换行时出问题: 内容中包含了换行的替换成空格
+    */
 
     private static final String SPLIT = ",";
     private static final String WIN_WRAP = "\r";
@@ -85,22 +87,22 @@ public class ExportCsv {
     }
 
     private static String handleCsvContent(String content) {
-        if (U.isNull(content)) {
+        if (U.isBlank(content)) {
             return U.EMPTY;
         }
-        if (U.isNotBlank(content)) {
-            if (content.contains(WIN_WRAP)) {
-                content = content.replace(WIN_WRAP, BLANK);
-            }
-            if (content.contains(WRAP)) {
-                content = content.replace(WRAP, SPACE);
-            }
-            if (content.contains(QUOTE)) {
-                content = content.replace(QUOTE, REPLACE_QUOTE);
-            }
-            if (content.contains(SPLIT) || content.contains(SPACE) || content.contains(QUOTE)) {
-                content = QUOTE + content + QUOTE;
-            }
+
+        if (content.contains(WIN_WRAP)) {
+            content = content.replace(WIN_WRAP, BLANK);
+        }
+        if (content.contains(WRAP)) {
+            content = content.replace(WRAP, SPACE);
+        }
+        boolean hasQuote = content.contains(QUOTE);
+        if (hasQuote) {
+            content = content.replace(QUOTE, REPLACE_QUOTE);
+        }
+        if (content.contains(SPLIT) || content.contains(SPACE) || hasQuote) {
+            content = QUOTE + content + QUOTE;
         }
         return content;
     }
