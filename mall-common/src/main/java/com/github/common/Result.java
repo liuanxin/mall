fc48ec02b354c1a49a65127c61e987f1a625bedd
TY <liu.anxin13@gmail.com>
2024-11-16 10:59:22 +0800

    fix rebuild

--- mall-common/src/main/java/com/github/common/Result.java
+++ mall-common/src/main/java/com/github/common/Result.java
@@ -1,13 +1,26 @@
 package com.github.common;
 
-/** union-type 结构体, 一次返回结果和异常, 正常数据和异常信息在调用方拿到结果后处理 */
-public record Result<T, E extends Throwable> (T value, E error, boolean isOk) {
+/**
+ * <pre>
+ * union-type 结构体, 一次返回结果和异常, 正常数据和异常信息在调用方拿到结果后处理
+ *
+ * 正常返回时: Result&lt;Xxx, Exception> r = Result.ok(xxx);
+ * 异常返回时: Result&lt;Xxx, Exception> r = Result.err(e);
+ *
+ * 用的时候:
+ * if (r.isError()) {
+ *     return Result.err(r.error());
+ * }
+ * Xxx x = r.data();
+ * </pre>
+ */
+public record Result<T, E extends Throwable> (T data, E error, boolean isError) {
 
-    public static <T, E extends Throwable> Result<T, E> ok(T value) {
-        return new Result<>(value, null, true);
+    public static <T, E extends Throwable> Result<T, E> ok(T data) {
+        return new Result<>(data, null, false);
     }
 
     public static <T, E extends Throwable> Result<T, E> err(E error) {
-        return new Result<>(null, error, false);
+        return new Result<>(null, error, true);
     }
 }
